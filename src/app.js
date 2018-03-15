e7ab9968daed8b28be0f17af94e8c0b8c4dffe5c
danbraj <danbraj@users.noreply.github.com>
2018-03-15 15:15:15 +0100

    add webpack

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
 
 const greet = require('./Example/Greetings.js');
-const path = require('path');
 
-const pathObj = path.parse(__filename);
-
-console.log(`${pathObj.base} wrote ${greet('Daniel')}`);
+console.log(greet('Daniel'));
