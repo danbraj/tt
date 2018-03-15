13908e0dedc4f0c3a64b2b666f5e9fb1aefd2a1a
danbraj <danbraj@users.noreply.github.com>
2018-03-15 23:31:10 +0100

    configurate webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
 
+const BUILD_DIR = path.resolve(__dirname, './dist');
+
 module.exports = {
   entry: './src/app.js',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: BUILD_DIR,
     filename: 'tt.bundle.js'
+  },
+  devServer: {
+    contentBase: BUILD_DIR,
+    compress: true,
+    port: 9000,
+    hot: true,
+    open: true
   }
 };
