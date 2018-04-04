dd3379c3c27f59caa535129dfef81e26696b9adc
danbraj <danbraj@users.noreply.github.com>
2018-04-05 01:09:09 +0200

    Change unit tests framework
    QUnit -> mocha

--- tests/example-test.js
+++ tests/example-test.js
@@ -1,16 +1,19 @@
+const assert = require('assert');
 const greet = require('./../src/Example/Greetings.js');
 
-QUnit.test('check if variable is function', function (assert) {
+describe('Example tests', function () {
 
-    assert.ok(typeof greet === 'function', 'greet should be a function');
+    it('should be function variable', function () {
+
+        assert.ok(typeof greet === 'function', 'greet should be a function');
+    });
+
+    it('should strings be the same in greet function', function () {
+
+        assert.equal(
+            greet('Martha'),
+            'Greetings Martha!',
+            'strings should be the same'
+        );
+    });
 });
-
-QUnit.test('check correctness greetings function', function (assert) {
-    
-    assert.equal(
-        greet('Martha'),
-        'Greetings Martha!',
-        'strings should be the same'
-    );
-});
-
