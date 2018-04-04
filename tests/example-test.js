8595369264ef24082863ff3c6f58eba38fb1bdb5
danbraj <danbraj@users.noreply.github.com>
2018-04-05 01:18:28 +0200

    Add 'chai' package

--- tests/example-test.js
+++ tests/example-test.js
@@ -1,19 +1,17 @@
 const assert = require('assert');
+const expect = require('chai').expect;
+
 const greet = require('./../src/Example/Greetings.js');
 
 describe('Example tests', function () {
 
     it('should be function variable', function () {
 
-        assert.ok(typeof greet === 'function', 'greet should be a function');
+        expect(greet).to.be.a('function');
     });
 
     it('should strings be the same in greet function', function () {
 
-        assert.equal(
-            greet('Martha'),
-            'Greetings Martha!',
-            'strings should be the same'
-        );
+        expect(greet('Martha')).to.equal('Greetings Martha!');
     });
 });
