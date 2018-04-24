190df004990c4503a41873c2ccba9e89537a5d04
danbraj <danbraj@users.noreply.github.com>
2018-04-24 21:17:43 +0200

    Play with test cases

--- test/numbering-test.js
+++ test/numbering-test.js
@@ -2,5 +2,31 @@ const expect = require('chai').expect;
 
 describe('Numbering module tests', function () {
 
-    // TODO:
+    const tCases = [
+        {
+            data: 1,
+            modifier: 2,
+            expected: '12'
+        },
+        {
+            data: 'a',
+            modifier: '2',
+            expected: 'a2'
+        },
+        {
+            data: 1,
+            modifier: 'a',
+            expected: '1a'
+        }
+    ]
+
+    const numberingTest = (tCase) => {
+        return () => {
+            expect(`${tCase.data}${tCase.modifier}`).to.equal(tCase.expected);;
+        }
+    }
+
+    tCases.forEach((tCase) => {
+        it('should strings be the same', numberingTest(tCase));
+    });
 });
